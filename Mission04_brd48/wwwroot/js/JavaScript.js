9789f85d8ca3fe2b9b90d848b654f94457b4a046
rylan-dunning <brdunning14@gmail.com>
2023-01-30 21:19:58 -0700

    finally fixed the output yeehaw

--- Mission04_brd48/wwwroot/js/JavaScript.js
+++ Mission04_brd48/wwwroot/js/JavaScript.js
@@ -7,7 +7,38 @@
     const exams = parseFloat($("#exams").val());
     const extracredit = parseFloat($("#extracredit").val());
 
-    const finalGrade = (assignments / 100) * 53.76 + (quizzes / 100) * 8.6 + (groupprojects / 100) * 5.38 + (intex / 100) * 10.75 + (exams / 100) * 21.51 + (extracredit / 100) * 0.81;
-    alert("Your final grade in this class is: " + finalGrade);
+    let gradeChar = "";
 
-})
+    const finalGrade = (assignments / 100) * 53.76 + (quizzes / 100) * 8.6 + (groupprojects / 100) * 5.38 + (intex / 100) * 10.75 + (exams / 100) * 21.51 + (extracredit / 100) * 0.81;
+
+    if (finalGrade >= 94) {
+        gradeChar = "A";
+    } else if (finalGrade >= 90) {
+        gradeChar = "A-";
+    } else if (finalGrade >= 87) {
+        gradeChar = "B+";
+    } else if (finalGrade >= 84) {
+        gradeChar = "B";
+    } else if (finalGrade >= 80) {
+        gradeChar = "B-";
+    } else if (finalGrade >= 77) {
+        gradeChar = "C+";
+    } else if (finalGrade >= 74) {
+        gradeChar = "C";
+    } else if (finalGrade >= 70) {
+        gradeChar = "C-";
+    } else if (finalGrade >= 67) {
+        gradeChar = "D+";
+    } else if (finalGrade >= 64) {
+        gradeChar = "D";
+    } else if (finalGrade >= 60) {
+        gradeChar = "D-";
+    } else {
+        gradeChar = "E";
+    }
+
+    $("#output").html("Your final grade is: " + finalGrade + " (" + gradeChar + ").");
+
+});
+
+
